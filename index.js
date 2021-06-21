7e5a84749bdae7605d2af023750b19f77c8ff1fa
Gabriel Lemache <lemache35@gmail.com>
2021-06-20 22:03:57 -0300

    working

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(cors())
+app.use(cors({origin:'localhost:3000', credentials:true}))
 
 mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/data", {useUnifiedTopology:true, useNewUrlParser:true, autoIndex:false}).then(() => {
     console.log("sucsess");
