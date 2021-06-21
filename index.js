17a618d243396fcca9eb6f0c17e10dc4bfd7d12d
Gabriel Lemache <lemache35@gmail.com>
2021-06-20 21:05:10 -0300

    teste cookie6

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(cors())
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
 
 mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/data", {useUnifiedTopology:true, useNewUrlParser:true, autoIndex:false}).then(() => {
     console.log("sucsess");
