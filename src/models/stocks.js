26b943fb31f05299401781dd2660ff022c9b3f49
Gabriel Lemache <lemache35@gmail.com>
2021-08-19 22:09:15 -0300

    stock data

--- src/models/stocks.js
+++ src/models/stocks.js
@@ -2,16 +2,16 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const Stocks = new Schema({
-    name:{
-        type: String,
-        required: true
-    },
     ticker:{
         type: String,
         required: true
     },
-    codigo:{
-        type: String,
+    open:{
+        type: Number,
+        required: true
+    },
+    close:{
+        type: Number,
         required: true
     },
     date: {
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const authcontorller = require('../controllers/authcontorller');
 const routes = express.Router();
+const stockData = require('../requests/stockData')
 
 
 routes.post('/register', authcontorller.store);
 routes.post('/auth', authcontorller.auth)
 routes.get('/cookiesGet', authcontorller.verifyJWT)
 routes.get('/logout', authcontorller.logout)
+routes.get('/data', stockData.getData)
 
 module.exports = routes;
