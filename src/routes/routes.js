4bb73fbac1e0c76b96b26c480ce21786bd8c7f0d
Gabriel Lemache <lemache35@gmail.com>
2021-08-27 23:30:58 -0300

    get pices requests

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const authcontorller = require('../controllers/authcontorller');
 const routes = express.Router();
-const GetVar = require('../requests/HighLowData')
-const stockdata = require('../requests/stockData')
-
+const getData = require('../requests/stockData')
 
 routes.post('/register', authcontorller.store);
 routes.post('/auth', authcontorller.auth)
 routes.get('/cookiesGet', authcontorller.verifyJWT)
 routes.get('/logout', authcontorller.logout)
 
-routes.get('/data', GetVar.GetVar)
-routes.get('/datav', stockdata.getData)
+routes.post('/datav', getData.getData)
 
 module.exports = routes;
